7ab3278f571094bc799c26c76f06fc6a98f9ea8d
Rugroso <133253941+Rugroso@users.noreply.github.com>
2025-02-22 20:32:09 -0800

    Status bar Added

--- src/global.gd
+++ src/global.gd
@@ -6,6 +6,20 @@ var stats: Stats
 var money: Money
 var day := 1
 var cash := 1000
+var members = {
+	"familiar_1": {
+		"name": "You",
+		"node": "res://scenes/stats/Member.tscn"
+	},
+	"familiar_2": {
+		"name": "Wife",
+		"node": "res://scenes/stats/Member.tscn"
+	},
+	"familiar_3": {
+		"name": "Son",
+		"node": "res://scenes/stats/Member.tscn"
+	},
+}
 
 func increase_day() -> void:
 	day += 1
