0405b6f1938fea03df236cc544e6a8489fb05186
Juan <jposorniot@gmail.com>
2025-02-23 15:22:56 -0800

    ends on 0 health

--- src/stat.gd
+++ src/stat.gd
@@ -10,25 +10,21 @@ var mid = preload("res://mats/mid.tres")
 var sad = preload("res://mats/sad.tres")
 var satisfied = preload("res://mats/satisfied.tres")
 
-
 func _ready():
 	$Label.text = sname
 	update_sprite(value)
-	$ProgressBar.value = value
 
 func update_sprite(health: int) -> void: 
+	$ProgressBar.value = value
 	if $MarginContainer/AnimatedSprite2D != null:
 		if health > 75: 
 			$MarginContainer/AnimatedSprite2D.sprite_frames = happy
-			$MarginContainer/AnimatedSprite2D.play()
 		elif health > 50:
 			$MarginContainer/AnimatedSprite2D.sprite_frames = satisfied
-			$MarginContainer/AnimatedSprite2D.play()
 		elif health > 25: 
 			$MarginContainer/AnimatedSprite2D.sprite_frames = mid
-			$MarginContainer/AnimatedSprite2D.play()
-		elif health >= 0: 
-			health=0;
-			Global.main.show_popup()
+		elif health > 0:
 			$MarginContainer/AnimatedSprite2D.sprite_frames = sad
-			$MarginContainer/AnimatedSprite2D.play()
+		else:
+			Global.main.show_popup()
+	$MarginContainer/AnimatedSprite2D.play()
